bdf3cbd417886574bea8f2704044a2f11b136d73
Sebastian Blask <blask@gmx.de>
2015-08-12 15:15:26 +0200

    Handle incomplete encoding(see Google preferences for example)

--- test/test-url.js
+++ test/test-url.js
@@ -9,34 +9,36 @@ var httpTargetDomain = "http://www.redirection.target.com/";
 var httpTargetUrl = httpTargetDomain + queryAndFragment;
 var httpTargetUrlEncoded = encodeURIComponent(httpTargetUrl);
 var httpTargetUrlDoubleEncoded = encodeURIComponent(httpTargetUrlEncoded);
+var httpTargetUrlIncompletelyEncoded = httpTargetDomain + encodeURIComponent(queryAndFragment);
 
 var wwwTargetDomain = "www.redirection.target.com/";
 var wwwTargetUrl = wwwTargetDomain + queryAndFragment;
 var wwwTargetUrlEncoded = encodeURIComponent(wwwTargetUrl);
 var wwwTargetUrlDoubleEncoded = encodeURIComponent(wwwTargetUrlEncoded);
+var wwwTargetUrlIncompletelyEncoded = wwwTargetDomain + encodeURIComponent(queryAndFragment);
 
 var noRedirectUrls = [
     httpTargetUrl,
     sourceHTTP + wwwTargetUrl.replace("www.", "www"),
-    sourceHTTP + "/login?continue=" + httpTargetUrl + "#some-fragment",
-    sourceHTTP + "/login?continue=" + httpTargetUrlEncoded + "#some-fragment",
-    sourceHTTP + "/login?continue=" + httpTargetUrlDoubleEncoded + "#some-fragment]",
+    sourceHTTP + "login?continue=" + httpTargetUrl + "#some-fragment",
+    sourceHTTP + "login?continue=" + httpTargetUrlEncoded + "#some-fragment",
+    sourceHTTP + "login?continue=" + httpTargetUrlDoubleEncoded + "#some-fragment]",
 ];
 var noRedirectTests = [for (url of noRedirectUrls) [url, url]];
 
 function pathTest(source, target, expected) {
     return [
         [source + target, expected],
-        [source + "/?" + target, expected],
+        [source + "?" + target, expected],
     ];
 }
 
 function queryTest(source, target, expected) {
     return [
-        [source + "/?target=" + target, expected],
-        [source + "/?target=" + target + "&some=parameter", expected],
-        [source + "/?target=" + target + ";some=parameter", expected],
-        [source + "/?target=" + target + "#some-fragment", expected],
+        [source + "?target=" + target, expected],
+        [source + "?target=" + target + "&some=parameter", expected],
+        [source + "?target=" + target + ";some=parameter", expected],
+        [source + "?target=" + target + "#some-fragment", expected],
     ];
 }
 
@@ -50,30 +52,38 @@ exports["test redirects"] = function(assert) {
     var tests =
         []
             .concat(noRedirectTests)
-            .concat(pathTest(sourceHTTP,  httpTargetUrl,              httpTargetUrl))
-            .concat(pathTest(sourceHTTP,  httpTargetUrlEncoded,       httpTargetUrl))
-            .concat(pathTest(sourceHTTP,  httpTargetUrlDoubleEncoded, httpTargetUrl))
-            .concat(pathTest(sourceHTTPS, httpTargetUrl,              httpTargetUrl))
-            .concat(pathTest(sourceHTTPS, httpTargetUrlEncoded,       httpTargetUrl))
-            .concat(pathTest(sourceHTTPS, httpTargetUrlDoubleEncoded, httpTargetUrl))
-            .concat(pathTest(sourceHTTP,  wwwTargetUrl,               "http://" + wwwTargetUrl))
-            .concat(pathTest(sourceHTTP,  wwwTargetUrlEncoded,        "http://" + wwwTargetUrl))
-            .concat(pathTest(sourceHTTP,  wwwTargetUrlDoubleEncoded,  "http://" + wwwTargetUrl))
-            .concat(pathTest(sourceHTTPS, wwwTargetUrl,               "http://" + wwwTargetUrl))
-            .concat(pathTest(sourceHTTPS, wwwTargetUrlEncoded,        "http://" + wwwTargetUrl))
-            .concat(pathTest(sourceHTTPS, wwwTargetUrlDoubleEncoded,  "http://" + wwwTargetUrl))
-            .concat(queryTest(sourceHTTP,  httpTargetUrl,              httpTargetDomain))
-            .concat(queryTest(sourceHTTP,  httpTargetUrlDoubleEncoded, httpTargetUrl))
-            .concat(queryTest(sourceHTTP,  httpTargetUrlEncoded,       httpTargetUrl))
-            .concat(queryTest(sourceHTTPS, httpTargetUrl,              httpTargetDomain))
-            .concat(queryTest(sourceHTTPS, httpTargetUrlDoubleEncoded, httpTargetUrl))
-            .concat(queryTest(sourceHTTPS, httpTargetUrlEncoded,       httpTargetUrl))
-            .concat(queryTest(sourceHTTP,  wwwTargetUrl,               "http://" + wwwTargetDomain))
-            .concat(queryTest(sourceHTTP,  wwwTargetUrlDoubleEncoded,  "http://" + wwwTargetUrl))
-            .concat(queryTest(sourceHTTP,  wwwTargetUrlEncoded,        "http://" + wwwTargetUrl))
-            .concat(queryTest(sourceHTTPS, wwwTargetUrl,               "http://" + wwwTargetDomain))
-            .concat(queryTest(sourceHTTPS, wwwTargetUrlDoubleEncoded,  "http://" + wwwTargetUrl))
-            .concat(queryTest(sourceHTTPS, wwwTargetUrlEncoded,        "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTP,   httpTargetUrl,                    httpTargetUrl))
+            .concat(pathTest(sourceHTTP,   httpTargetUrlDoubleEncoded,       httpTargetUrl))
+            .concat(pathTest(sourceHTTP,   httpTargetUrlEncoded,             httpTargetUrl))
+            .concat(pathTest(sourceHTTP,   httpTargetUrlIncompletelyEncoded, httpTargetUrl))
+            .concat(pathTest(sourceHTTPS,  httpTargetUrl,                    httpTargetUrl))
+            .concat(pathTest(sourceHTTPS,  httpTargetUrlDoubleEncoded,       httpTargetUrl))
+            .concat(pathTest(sourceHTTPS,  httpTargetUrlEncoded,             httpTargetUrl))
+            .concat(pathTest(sourceHTTPS,  httpTargetUrlIncompletelyEncoded, httpTargetUrl))
+            .concat(pathTest(sourceHTTP,   wwwTargetUrl,                     "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTP,   wwwTargetUrlDoubleEncoded,        "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTP,   wwwTargetUrlEncoded,              "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTP,   wwwTargetUrlIncompletelyEncoded,  "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTPS,  wwwTargetUrl,                     "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTPS,  wwwTargetUrlEncoded,              "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTPS,  wwwTargetUrlDoubleEncoded,        "http://" + wwwTargetUrl))
+            .concat(pathTest(sourceHTTPS,  wwwTargetUrlIncompletelyEncoded,  "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTP,  httpTargetUrl,                    httpTargetUrl.slice(0, httpTargetUrl.lastIndexOf("?"))))
+            .concat(queryTest(sourceHTTP,  httpTargetUrlDoubleEncoded,       httpTargetUrl))
+            .concat(queryTest(sourceHTTP,  httpTargetUrlEncoded,             httpTargetUrl))
+            .concat(queryTest(sourceHTTP,  httpTargetUrlIncompletelyEncoded, httpTargetUrl))
+            .concat(queryTest(sourceHTTPS, httpTargetUrl,                    httpTargetUrl.slice(0, httpTargetUrl.lastIndexOf("?"))))
+            .concat(queryTest(sourceHTTPS, httpTargetUrlDoubleEncoded,       httpTargetUrl))
+            .concat(queryTest(sourceHTTPS, httpTargetUrlEncoded,             httpTargetUrl))
+            .concat(queryTest(sourceHTTPS, httpTargetUrlIncompletelyEncoded, httpTargetUrl))
+            .concat(queryTest(sourceHTTP,  wwwTargetUrl,                     "http://" + wwwTargetUrl.slice(0, wwwTargetUrl.lastIndexOf("?"))))
+            .concat(queryTest(sourceHTTP,  wwwTargetUrlDoubleEncoded,        "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTP,  wwwTargetUrlEncoded,              "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTP,  wwwTargetUrlIncompletelyEncoded,  "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTPS, wwwTargetUrl,                     "http://" + wwwTargetUrl.slice(0, wwwTargetUrl.lastIndexOf("?"))))
+            .concat(queryTest(sourceHTTPS, wwwTargetUrlDoubleEncoded,        "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTPS, wwwTargetUrlEncoded,              "http://" + wwwTargetUrl))
+            .concat(queryTest(sourceHTTPS, wwwTargetUrlIncompletelyEncoded,  "http://" + wwwTargetUrl))
             .concat(twoTargetsTests)
             .concat([]);
     for (var index = 0; index < tests.length; index++) {
