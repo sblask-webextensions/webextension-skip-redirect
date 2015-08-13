0ae6fa3da7be0ec7e664d3614b61dccb8c8d9836
Sebastian Blask <blask@gmx.de>
2015-08-13 11:37:19 +0200

    Support recursion

--- test/test-url.js
+++ test/test-url.js
@@ -1,14 +1,14 @@
 const url = require("./url");
 
-var queryAndFragment = "?some=parameter&some-other=parameter;another=parameter#some-fragment";
+const queryAndFragment = "?some=parameter&some-other=parameter;another=parameter#some-fragment";
 
-var wwwTargetUrl = "www.redirection.target.com/" + queryAndFragment;
-var wwwTargetUrlEncoded = encodeURIComponent(wwwTargetUrl);
-var wwwTargetUrlDoubleEncoded = encodeURIComponent(wwwTargetUrlEncoded);
+const wwwTargetUrl = "www.redirection.target.com/" + queryAndFragment;
+const wwwTargetUrlEncoded = encodeURIComponent(wwwTargetUrl);
+const wwwTargetUrlDoubleEncoded = encodeURIComponent(wwwTargetUrlEncoded);
 
-var someTargetUrl = "some.www.redirection.target.com/" + queryAndFragment;
-var someTargetUrlEncoded = encodeURIComponent(someTargetUrl);
-var someTargetUrlDoubleEncoded = encodeURIComponent(someTargetUrlEncoded);
+const someTargetUrl = "some.www.redirection.target.com/" + queryAndFragment;
+const someTargetUrlEncoded = encodeURIComponent(someTargetUrl);
+const someTargetUrlDoubleEncoded = encodeURIComponent(someTargetUrlEncoded);
 
 exports["test skipping to urls in querystring"] = function(assert) {
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "http://" +             someTargetUrl),              "http://"  + someTargetUrl);
@@ -36,13 +36,23 @@ exports["test skipping to urls in query parameter"] = function(assert) {
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" + "http://" +             someTargetUrlEncoded),                     "http://" + someTargetUrl);
 
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrl),                            "http://"  + wwwTargetUrl);
-    assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrl + "?some=parameter"),        "http://"  + wwwTargetUrl);
+    assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrl +        "?some=parameter"), "http://"  + wwwTargetUrl);
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrlEncoded + "&some=parameter"), "http://"  + wwwTargetUrl);
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrlEncoded + ";some=parameter"), "http://"  + wwwTargetUrl);
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrlEncoded + "#some-fragment"),  "http://"  + wwwTargetUrl);
     assert.equal(url.getRedirectTarget("http://"  + "www.some.website.com" + "/" + "?target=" +                          wwwTargetUrlDoubleEncoded),               "http://"  + wwwTargetUrl);
 };
 
+exports["test recursion"] = function(assert) {
+    let wwwTargetUrlOne =  wwwTargetUrl + "-ONE";
+    let wwwTargetUrlTwo =  wwwTargetUrl + "-TWO";
+    assert.equal(url.getRedirectTarget("http://www.some.website.com/" +                                             wwwTargetUrlOne + "/" +                                       wwwTargetUrlTwo),   "http://" + wwwTargetUrlTwo);
+    assert.equal(url.getRedirectTarget("http://www.some.website.com/" +                                 "http://" + wwwTargetUrlOne + "/" +                           "http://" + wwwTargetUrlTwo),   "http://" + wwwTargetUrlTwo);
+
+    assert.equal(url.getRedirectTarget("http://www.some.website.com/" + "?target=" +                                wwwTargetUrlOne + "&target=" +                                wwwTargetUrlTwo),   "http://" + wwwTargetUrlTwo);
+    assert.equal(url.getRedirectTarget("http://www.some.website.com/" + "?target=" +                    "http://" + wwwTargetUrlOne + "&target=" +                    "http://" + wwwTargetUrlTwo),   "http://" + wwwTargetUrlTwo);
+    assert.equal(url.getRedirectTarget("http://www.some.website.com/" + "?target=" + encodeURIComponent("http://" + wwwTargetUrlOne + "&target=" + encodeURIComponent("http://" + wwwTargetUrlTwo))), "http://" + wwwTargetUrlTwo);
+};
 
 exports["test exceptions to skipping"] = function(assert) {
     let noRedirectUrls = [
