1150591216616cabf02ecc1f1478a59af1a7bb5c
Sebastian Blask <blask@gmx.de>
2015-11-01 23:48:55 +0100

    Test onUnload as well

--- test/test-imports.js
+++ test/test-imports.js
@@ -1,6 +1,7 @@
 exports["test install"] = function(assert) {
     const index = require("../index");
-    assert.equal(index.main({}), undefined);
+    assert.equal(index.main({loadReason: "install"}), undefined);
+    assert.equal(index.onUnload("uninstall"), undefined);
 };
 
 require("sdk/test").run(exports);
