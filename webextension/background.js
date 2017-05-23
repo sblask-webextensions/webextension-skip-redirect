fc6b1cc13e9c6e0911c6e910a9283ba94393beba
Sebastian Blask <blask@gmx.de>
2017-05-24 01:47:29 +0200

    Implement as webextension

---
+++ webextension/background.js
@@ -0,0 +1,115 @@
+/* global url */
+const MODE = "mode";
+const MODE_OFF = "off";
+const MODE_BLACKLIST = "blacklist";
+
+const BLACKLIST = "blacklist";
+
+const ICON           = "icon.svg";
+const ICON_OFF       = "icon-off.svg";
+const ICON_BLACKLIST = "icon-blacklist.svg";
+
+const GLOBAL_BLACKLIST = [
+    "/abp",
+    "/account",
+    "/auth",
+    "/cookie",
+    "/download",
+    "/login",
+    "/logout",
+    "/preferences",
+    "/profile",
+    "/register",
+    "/signin",
+    "/signoff",
+    "/signon",
+    "/signout",
+    "/signup",
+    "/subscribe",
+    "/verification",
+];
+
+let blacklist = [];
+
+browser.storage.local.get([MODE, BLACKLIST])
+    .then(
+        (result) => {
+            if (result[MODE] === undefined) {
+                browser.storage.local.set({[MODE]: MODE_BLACKLIST});
+            } else if (result[MODE] === MODE_OFF) {
+                disableSkipping();
+            } else {
+                enableSkipping();
+            }
+
+            if (result[BLACKLIST] === undefined) {
+                browser.storage.local.set({[BLACKLIST]: GLOBAL_BLACKLIST});
+            } else {
+                blacklist = result[BLACKLIST];
+            }
+        }
+    );
+
+browser.storage.onChanged.addListener(
+    (changes) => {
+        if (changes[MODE]) {
+            if (changes[MODE].newValue === MODE_OFF) {
+                disableSkipping();
+            } else {
+                enableSkipping();
+            }
+        }
+
+        if (changes[BLACKLIST]) {
+            blacklist = changes[BLACKLIST].newValue;
+        }
+    }
+);
+
+function enableSkipping() {
+    browser.webRequest.onBeforeRequest.addListener(
+        maybeRedirect,
+        {urls: ["<all_urls>"], types: ["main_frame"]},
+        ["blocking"]
+    );
+
+    browser.browserAction.setIcon({path: ICON_BLACKLIST});
+    browser.browserAction.setTitle({title: "Skip Redirect is enabled, click to disable"});
+}
+
+function disableSkipping() {
+    browser.webRequest.onBeforeRequest.removeListener(maybeRedirect);
+
+    browser.browserAction.setIcon({path: ICON_OFF});
+    browser.browserAction.setTitle({title: "Skip Redirect is disabled, click to enable"});
+}
+
+function maybeRedirect(requestDetails) {
+    if (requestDetails.tabId === -1 || requestDetails.method === "POST") {
+        return;
+    }
+
+    if (new RegExp("(" + blacklist.join("|") + ")").test(requestDetails.url)) {
+        return;
+    }
+
+    const redirectTarget = url.getRedirectTarget(requestDetails.url);
+    if (redirectTarget == requestDetails.url) {
+        return;
+    }
+
+    notifySkip(requestDetails.url, redirectTarget);
+
+    return {
+        redirectUrl: redirectTarget,
+    };
+}
+
+function notifySkip(from, to) {
+    browser.notifications.create("notify-skip", {
+        type: "basic",
+        iconUrl: browser.extension.getURL(ICON),
+        title: "Skipped Redirect",
+        message: from + "\n->\n" + to,
+    });
+}
---
+++ webextension/options/options.js
@@ -0,0 +1,22 @@
+function restoreOptions() {
+    browser.storage.local.get([
+        "mode",
+        "blacklist",
+    ], result => {
+        document.querySelector("#blacklist").value = result.blacklist.join("\n");
+        document.querySelector("#mode" + result.mode.charAt(0).toUpperCase() + result.mode.slice(1)).checked = "checked";
+    });
+}
+
+function saveOptions(event) {
+    event.preventDefault();
+    browser.storage.local.set({
+        blacklist: document.querySelector("#blacklist").value.split("\n"),
+        mode: document.querySelector("#modeOff").checked && "off" || document.querySelector("#modeBlacklist").checked && "blacklist",
+    });
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
+
+browser.storage.onChanged.addListener(restoreOptions);
