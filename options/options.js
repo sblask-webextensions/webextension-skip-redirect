1f447239e8ef34b45b65c7ee94f4e44e0fbcca7e
Sebastian Blask <blask@gmx.de>
2017-07-23 00:12:22 +0200

    Add fallback if translation is not found

--- options/options.js
+++ options/options.js
@@ -27,10 +27,11 @@ function enableAutosave() {
 
 function loadTranslations() {
     for (let element of document.querySelectorAll("[data-i18n]")) {
-        if (typeof browser === "undefined") {
+        let translationKey = element.getAttribute("data-i18n");
+        if (typeof browser === "undefined" || !browser.i18n.getMessage(translationKey)) {
             element.textContent = element.getAttribute("data-i18n");
         } else {
-            element.innerHTML = browser.i18n.getMessage(element.getAttribute("data-i18n"));
+            element.innerHTML = browser.i18n.getMessage(translationKey);
         }
     }
 }
