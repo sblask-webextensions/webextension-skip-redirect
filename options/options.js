71b008149e6846b7eab0d5567e01a64b7a9a98d9
Sebastian Blask <blask@gmx.de>
2020-07-27 16:08:46 +1200

    Highlight regular expression errors

--- options/options.js
+++ options/options.js
@@ -17,6 +17,7 @@ const ELEMENT_MODE_NO_SKIP_LIST = "mode-no-skip-list";
 const ELEMENT_MODE_SKIP_LIST = "mode-skip-list";
 
 const ELEMENT_NO_SKIP_LIST = "no-skip-list";
+const ELEMENT_NO_SKIP_LIST_ERROR = "no-skip-list-error";
 const ELEMENT_SKIP_LIST = "skip-list";
 const ELEMENT_NOTIFICATION_POPUP_ENABLED = "notification-popup-eneabled";
 const ELEMENT_NOTIFICATION_DURATION = "notification-duration";
@@ -32,7 +33,10 @@ function restoreOptions() {
         OPTION_SKIP_REDIRECTS_TO_SAME_DOMAIN,
     ]).then(
         result => {
-            setTextValue(ELEMENT_NO_SKIP_LIST, result[OPTION_NO_SKIP_LIST].join("\n"));
+            const noSkipList = result[OPTION_NO_SKIP_LIST];
+            maybeHighlightError(noSkipList);
+
+            setTextValue(ELEMENT_NO_SKIP_LIST, noSkipList.join("\n"));
             setTextValue(ELEMENT_SKIP_LIST, result[OPTION_SKIP_LIST].join("\n"));
             setBooleanValue(ELEMENT_NOTIFICATION_POPUP_ENABLED, result[OPTION_NOTIFICATION_POPUP_ENABLED]);
             setTextValue(ELEMENT_NOTIFICATION_DURATION, result[OPTION_NOTIFICATION_DURATION]);
@@ -85,10 +89,47 @@ function setBooleanValue(elementID, newValue) {
     document.getElementById(elementID).checked = newValue;
 }
 
+function getRegExpError(noSkipList) {
+    for(const line of noSkipList) {
+        try {
+            new RegExp(line);
+        } catch(exception) {
+            return {
+                line,
+                message: exception.message,
+            };
+        }
+    }
+    return null;
+}
+
+function maybeHighlightError(noSkipList) {
+    const noSkipListElement = document.querySelector(`#${ELEMENT_NO_SKIP_LIST}`);
+    const noSkipListErrorSpan = document.querySelector(`#${ELEMENT_NO_SKIP_LIST_ERROR}`);
+    const noSkipListError = getRegExpError(noSkipList);
+    if (noSkipListError) {
+        const {line, message} = noSkipListError;
+        noSkipListElement.classList.add("error");
+
+        if (message.includes(line)) {
+            noSkipListErrorSpan.innerText = message;
+        } else {
+            noSkipListErrorSpan.innerText = `${line}: ${message}`;
+        }
+    } else {
+        noSkipListElement.classList.remove("error");
+        noSkipListErrorSpan.innerText = "";
+    }
+}
+
 function saveOptions(event) {
     event.preventDefault();
+    const noSkipList = document.querySelector(`#${ELEMENT_NO_SKIP_LIST}`).value.split("\n");
+
+    maybeHighlightError(noSkipList);
+
     browser.storage.local.set({
-        [OPTION_NO_SKIP_LIST]: document.querySelector(`#${ELEMENT_NO_SKIP_LIST}`).value.split("\n"),
+        [OPTION_NO_SKIP_LIST]: noSkipList,
         [OPTION_SKIP_LIST]: document.querySelector(`#${ELEMENT_SKIP_LIST}`).value.split("\n"),
         [OPTION_MODE]:
             document.querySelector(`#${ELEMENT_MODE_OFF}`).checked && OPTION_MODE_OFF
