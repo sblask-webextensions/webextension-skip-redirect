f803a368c2f9510c708ade407a597864f0abb176
garywill <garywill@disroot.org>
2022-11-12 08:26:03 +0800

    use tab.update instead of redirectUrl to vanish referer

--- background.js
+++ background.js
@@ -381,9 +381,7 @@ function maybeRedirect(requestDetails) {
     prepareToolbarContextMenu(requestDetails.url);
     notifySkip(requestDetails.url, redirectTarget);
 
-    return {
-        redirectUrl: redirectTarget,
-    };
+    browser.tabs.update(requestDetails.tabId, {url: redirectTarget});
 }
 
 function prepareToolbarContextMenu(from) {
